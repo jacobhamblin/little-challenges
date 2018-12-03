7f5e6b6f236a72e1de29913419d216cdc1a00521
Jacob Hamblin <jacob@hamblin.cc>
2018-12-02 21:27:24 -0700

    seems solved

--- tests/test.py
+++ tests/test.py
@@ -24,3 +24,19 @@ def test_string_length_two():
     expect_equal(num_interpretations.decode('21'), 2)
     expect_equal(num_interpretations.decode('27'), 1)
     expect_equal(num_interpretations.decode('02'), 1)
+
+
+def test_string_length_three():
+    expect_equal(num_interpretations.decode('582'), 1)
+    expect_equal(num_interpretations.decode('132'), 2)
+    expect_equal(num_interpretations.decode('212'), 3)
+    expect_equal(num_interpretations.decode('272'), 1)
+    expect_equal(num_interpretations.decode('024'), 2)
+
+
+def test_string_length_four():
+    expect_equal(num_interpretations.decode('5821'), 2)
+    expect_equal(num_interpretations.decode('1322'), 3)
+    expect_equal(num_interpretations.decode('2121'), 4)
+    expect_equal(num_interpretations.decode('2727'), 1)
+    expect_equal(num_interpretations.decode('0218'), 3)
