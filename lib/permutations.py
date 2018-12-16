8a225aa472d832e3931a806187b16865c21da15e
Jacob Hamblin <jacob@hamblin.cc>
2018-12-15 19:50:44 -0700

    so ugly

--- lib/permutations.py
+++ lib/permutations.py
@@ -1,3 +1,21 @@
 def recurse(input):
     if not len(input):
-        return input or [input]
+        return []
+    if len(input) == 1:
+        return list(input)
+    head = list(input[0])
+    rest = input[1]
+    if not type(input) == list:
+        rest = input[1:len(input)]
+    if not len(rest):
+        return head
+    next = rest[0]
+    permutations = []
+    for existing_perm in head:
+        new_perms = []
+        for i in xrange(0, len(existing_perm) + 1):
+            new_perm = list(existing_perm)
+            new_perm.insert(i, next)
+            new_perms.append(''.join(new_perm))
+        permutations += new_perms
+    return recurse([permutations, rest[1:len(rest)]])
--- tests/test_permutations.py
+++ tests/test_permutations.py
@@ -3,4 +3,29 @@ from . import helpers
 
 
 def test_empty_string():
-    helpers.expect_equal(permutations.recurse(''), [''])
+    helpers.expect_equal(permutations.recurse(''), [])
+
+def test_length_one():
+    helpers.expect_equal(permutations.recurse('a'), ['a'])
+
+def test_length_two():
+    helpers.expect_equal(
+        sorted(permutations.recurse('ab')),
+        sorted(['ab', 'ba'])
+    )
+
+def test_length_three():
+    helpers.expect_equal(
+        sorted(permutations.recurse('abc')),
+        sorted(['abc', 'bac', 'acb', 'bca', 'cab', 'cba'])
+    )
+
+def test_length_four():
+    helpers.expect_equal(
+        sorted(permutations.recurse('dceq')),
+        sorted([
+            'cdeq', 'cdqe', 'cedq', 'ceqd', 'cqde', 'cqed', 'dceq', 'dcqe',
+            'decq', 'deqc', 'dqce', 'dqec', 'ecdq', 'ecqd', 'edcq', 'edqc',
+            'eqcd', 'eqdc', 'qcde', 'qced', 'qdce', 'qdec', 'qecd', 'qedc',
+        ])
+    )
