ea7b5cb8374c525651164d3010bd2bf7619f79d8
Jacob Hamblin <jacob@hamblin.cc>
2018-12-22 01:15:29 -0700

    wip

--- lib/change.py
+++ lib/change.py
@@ -5,12 +5,16 @@ def get_combinations(value, coins, coins_used):
     for index, coin in enumerate(coins):
         if (value / coin) > 1:
             coins_used[index] += 1
-            combinations += get_combinations(value - coin, coins, coins_used)
-    return combinations
+            combination = get_combinations(value - coin, coins, coins_used)
+            if combination:
+                combinations.append(combination)
+    if len(combinations):
+        return combinations
 
 
 def fewest_coins(value, coins):
     coins_used = [0 for coin in coins]
+    print(coins_used)
     combinations = get_combinations(value, coins, coins_used)
     sum = lambda x, y: x + y
     sums = [reduce(sum, combination) for combination in combinations]
