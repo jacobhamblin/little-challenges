ce3ae73af1798712488c19d9831c8e619501ee08
Jacob Hamblin <jacob@hamblin.cc>
2018-12-22 00:46:32 -0700

    some tests

--- lib/change.py
+++ lib/change.py
@@ -1,3 +1,24 @@
+def get_combinations(value, coins, coins_used):
+    combinations = []
+    if value == 0:
+        return coins_used
+    for index, coin in enumerate(coins):
+        if (value / coin) > 1:
+            coins_used[index] += 1
+            combinations += get_combinations(value - coin, coins, coins_used)
+    return combinations
+
+
 def fewest_coins(value, coins):
-    dispensed = []
-    return dispensed
+    coins_used = [0 for coin in coins]
+    combinations = get_combinations(value, coins, coins_used)
+    sum = lambda x, y: x + y
+    sums = [reduce(sum, combination) for combination in combinations]
+    minimum_count = min(sums)
+    for index, sum in enumerate(sums):
+        if (sum) == minimum_count:
+            return combinations[index]
+
+
+
+
--- tests/test_change.py
+++ tests/test_change.py
@@ -1,6 +1,25 @@
 from lib import change
 from . import helpers
 
+
 def test_no_change():
     helpers.expect_equal(change.fewest_coins(0, []), [])
     helpers.expect_equal(change.fewest_coins(0, [1, 5]), [])
+    helpers.expect_equal(change.fewest_coins(-5, [1, 5]), [])
+
+
+def test_up_to_one_of_each_coin():
+    helpers.expect_equal(change.fewest_coins(10, [1, 5, 10]), [0, 0, 1])
+    helpers.expect_equal(change.fewest_coins(15, [1, 5, 10]), [0, 1, 1])
+    helpers.expect_equal(change.fewest_coins(25, [1, 5, 10, 25]), [0, 0, 0, 1])
+
+
+def test_multiple_of_a_coin():
+    helpers.expect_equal(change.fewest_coins(29, [1, 5, 25]), [4, 0, 1])
+    helpers.expect_equal(change.fewest_coins(23, [1, 5, 10]), [3, 0, 2])
+    helpers.expect_equal(change.fewest_coins(18, [1, 5, 10]), [3, 1, 1])
+
+
+def test_strange_coins():
+    helpers.expect_equal(change.fewest_coins(13, [3, 6, 7]), [2, 0, 1])
+    helpers.expect_equal(change.fewest_coins(24, [5, 8, 12]), [0, 0, 2])
