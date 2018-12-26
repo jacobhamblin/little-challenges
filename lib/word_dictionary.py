2cad8569961397cf1a20958fbfc2f9a7510bb81f
Jacob Hamblin <jacob@hamblin.cc>
2018-12-26 13:15:07 -0700

    longest word, some tests

---
+++ lib/word_dictionary.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+from copy import copy
+
+def longest_word(string, list_of_words):
+    longest_length = 0
+    word_index = -1
+    if not len(list_of_words):
+        return ''
+    available_letters = defaultdict(lambda: 0)
+    for index in xrange(0, len(string)):
+        available_letters[string[index]] += 1
+    for index, word in enumerate(list_of_words):
+        if len(word) > longest_length:
+            word_works = True
+            available_letters_copy = copy(available_letters)
+            for char in word:
+                available_letters_copy[char] -= 1
+                if available_letters_copy[char] < 0:
+                    word_works = False
+                    break
+            if word_works:
+                longest_length = len(word)
+                word_index = index
+    if word_index > -1:
+        return list_of_words[word_index]
+    return ''
---
+++ tests/test_word_dictionary.py
@@ -0,0 +1,28 @@
+from . import helpers
+from lib import word_dictionary
+
+
+def test_longest_word_returns_empty_string_if_no_matching_words():
+    helpers.expect_equal(word_dictionary.longest_word('asd', []), '')
+    helpers.expect_equal(word_dictionary.longest_word('asd', ['ase']), '')
+
+
+def test_longest_word_no_duplicates():
+    helpers.expect_equal(
+        word_dictionary.longest_word('asd', ['sad', 'as', 'fast']),
+        'sad'
+    )
+    helpers.expect_equal(
+        word_dictionary.longest_word(
+            'naromd',
+            ['donar', 'moar', 'dan', 'random', 'rockstar']
+        ),
+        'random'
+    )
+
+
+def test_longest_word_duplicates():
+    helpers.expect_equal(
+        word_dictionary.longest_word('lilm', ['lim', 'mill']),
+        'mill'
+    )
