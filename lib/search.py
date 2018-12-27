43317223f101a6f05bfc8ebfba5e5499f3410a0a
Jacob Hamblin <jacob@hamblin.cc>
2018-12-27 15:30:30 -0700

    pass tests

--- lib/search.py
+++ lib/search.py
@@ -1,2 +1,8 @@
 def binary(list, value):
-    return 0
+    mid = len(list)/2
+    if list[mid] == value:
+        return mid
+    if list[mid] > value:
+        return binary(list[0:mid], value)
+    else:
+        return mid + 1 + binary(list[mid + 1:len(list)], value)
--- tests/test_search.py
+++ tests/test_search.py
@@ -6,3 +6,10 @@ def test_binary_search_small_array():
     helpers.expect_equal(search.binary([1,2,3], 2), 1)
     helpers.expect_equal(search.binary([18,20,71], 18), 0)
     helpers.expect_equal(search.binary([4,7,9], 9), 2)
+
+
+def test_binary_search_larger_array():
+    helpers.expect_equal(search.binary([1,2,3,4,5], 2), 1)
+    helpers.expect_equal(search.binary([8,11,14,16,19], 16), 3)
+    helpers.expect_equal(search.binary([1,3,5,7,9], 3), 1)
+    helpers.expect_equal(search.binary([1,3,4,6,7,9,10], 10), 6)
