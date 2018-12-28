0c650192892ab1ef217bf9c4a69bb9aba2efba8f
Jacob Hamblin <jacob@hamblin.cc>
2018-12-28 00:03:55 -0700

    simple implementation

--- lib/sort.py
+++ lib/sort.py
@@ -1,2 +1,7 @@
 def quicksort(list):
-    return list
+    if len(list) < 2:
+        return list
+    pivot = list[len(list) / 2]
+    less = [num for num in list if num < pivot]
+    more = [num for num in list if num > pivot]
+    return quicksort(less) + [pivot] * list.count(pivot) + quicksort(more)
--- tests/test_sort.py
+++ tests/test_sort.py
@@ -5,9 +5,14 @@ from lib import sort
 def test_quicksort_sorts_small_list():
     helpers.expect_equal(sort.quicksort([5,4]), [4,5])
     helpers.expect_equal(sort.quicksort([3,1,2]), [1,2,3])
-    helpers.expect_equal(sort.quicksort([4,18,2]), [2,4,18])
+    helpers.expect_equal(sort.quicksort([4.2,18.9,2.1]), [2.1,4.2,18.9])
     helpers.expect_equal(sort.quicksort([300,9,1]), [1,9,300])
     helpers.expect_equal(sort.quicksort([1,4,8,3]), [1,3,4,8])
+
+
+def test_quicksort_sorts_duplicates():
+    test_arr = [3,1,5,5,2,8,6,5]
+    helpers.expect_equal(sort.quicksort(test_arr), sorted(test_arr))
 
 
 def test_quicksort_sorts_larger_lists():
@@ -15,5 +20,5 @@ def test_quicksort_sorts_larger_lists():
     helpers.expect_equal(sort.quicksort(test_arr), sorted(test_arr))
     test_arr = [9,4,2,1,23,5,43,89,0,-12,-500]
     helpers.expect_equal(sort.quicksort(test_arr), sorted(test_arr))
-    test_arr = [-25, 0, 9, 2, -6, 13, 18]
+    test_arr = [-25, 0, 9.3, 2.1, 2.04, -6, 13, 18]
     helpers.expect_equal(sort.quicksort(test_arr), sorted(test_arr))
