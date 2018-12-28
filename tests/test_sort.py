b3edc308bb145be64e581181e4461b2a4e3c79ca
Jacob Hamblin <jacob@hamblin.cc>
2018-12-28 00:13:02 -0700

    refactor to ease testing more sorting algorithms

--- tests/test_sort.py
+++ tests/test_sort.py
@@ -2,23 +2,29 @@ from . import helpers
 from lib import sort
 
 
+SORTING_ALGORITHMS = [sort.quicksort]
+
+
 def test_quicksort_sorts_small_list():
-    helpers.expect_equal(sort.quicksort([5,4]), [4,5])
-    helpers.expect_equal(sort.quicksort([3,1,2]), [1,2,3])
-    helpers.expect_equal(sort.quicksort([4.2,18.9,2.1]), [2.1,4.2,18.9])
-    helpers.expect_equal(sort.quicksort([300,9,1]), [1,9,300])
-    helpers.expect_equal(sort.quicksort([1,4,8,3]), [1,3,4,8])
+    for function in SORTING_ALGORITHMS:
+        helpers.expect_equal(function([5,4]), [4,5])
+        helpers.expect_equal(function([3,1,2]), [1,2,3])
+        helpers.expect_equal(function([4.2,18.9,2.1]), [2.1,4.2,18.9])
+        helpers.expect_equal(function([300,9,1]), [1,9,300])
+        helpers.expect_equal(function([1,4,8,3]), [1,3,4,8])
 
 
 def test_quicksort_sorts_duplicates():
-    test_arr = [3,1,5,5,2,8,6,5]
-    helpers.expect_equal(sort.quicksort(test_arr), sorted(test_arr))
+    for function in SORTING_ALGORITHMS:
+        test_arr = [3,1,5,5,2,8,6,5]
+        helpers.expect_equal(function(test_arr), sorted(test_arr))
 
 
 def test_quicksort_sorts_larger_lists():
-    test_arr = [3,1,5,2,8,6]
-    helpers.expect_equal(sort.quicksort(test_arr), sorted(test_arr))
-    test_arr = [9,4,2,1,23,5,43,89,0,-12,-500]
-    helpers.expect_equal(sort.quicksort(test_arr), sorted(test_arr))
-    test_arr = [-25, 0, 9.3, 2.1, 2.04, -6, 13, 18]
-    helpers.expect_equal(sort.quicksort(test_arr), sorted(test_arr))
+    for function in SORTING_ALGORITHMS:
+        test_arr = [3,1,5,2,8,6]
+        helpers.expect_equal(function(test_arr), sorted(test_arr))
+        test_arr = [9,4,2,1,23,5,43,89,0,-12,-500]
+        helpers.expect_equal(function(test_arr), sorted(test_arr))
+        test_arr = [-25, 0, 9.3, 2.1, 2.04, -6, 13, 18]
+        helpers.expect_equal(function(test_arr), sorted(test_arr))
