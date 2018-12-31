4f6a9d137cb880c3f600aa9b0731e33b37515c3c
Jacob Hamblin <jacob@hamblin.cc>
2018-12-30 20:22:24 -0700

    handle missing value

--- lib/search.py
+++ lib/search.py
@@ -1,8 +1,13 @@
 def binary(list, value):
+    if not len(list):
+        return -1
     mid = len(list)/2
     if list[mid] == value:
         return mid
     if list[mid] > value:
         return binary(list[0:mid], value)
     else:
-        return mid + 1 + binary(list[mid + 1:len(list)], value)
+        rest = binary(list[mid + 1:len(list)], value)
+        if rest is -1:
+            return -1
+        return mid + 1 + rest
--- tests/test_search.py
+++ tests/test_search.py
@@ -2,6 +2,11 @@ from . import helpers
 from lib import search
 
 
+def test_missing_value():
+    helpers.expect_equal(search.binary([1,2,3], 5), -1)
+    helpers.expect_equal(search.binary([], 1), -1)
+
+
 def test_binary_search_small_array():
     helpers.expect_equal(search.binary([1,2,3], 2), 1)
     helpers.expect_equal(search.binary([18,20,71], 18), 0)
