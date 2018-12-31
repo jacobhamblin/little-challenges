fa497c0ad88c4c793247a0212bfa5b306de3dd9a
Jacob Hamblin <jacob@hamblin.cc>
2018-12-30 20:17:06 -0700

    merge sort

--- lib/sort.py
+++ lib/sort.py
@@ -1,7 +1,37 @@
-def quicksort(list):
-    if len(list) < 2:
-        return list
-    pivot = list[len(list) / 2]
-    less = [num for num in list if num < pivot]
-    more = [num for num in list if num > pivot]
-    return quicksort(less) + [pivot] * list.count(pivot) + quicksort(more)
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    pivot = array[len(array) / 2]
+    less = [num for num in array if num < pivot]
+    more = [num for num in array if num > pivot]
+    return quicksort(less) + [pivot] * array.count(pivot) + quicksort(more)
+
+
+def merge(left, right):
+    result = []
+    left_index = 0
+    right_index = 0
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] <= right[right_index]:
+            result.append(left[left_index])
+            left_index += 1
+        else:
+            result.append(right[right_index])
+            right_index += 1
+    if left_index < len(left):
+        result += left[left_index:]
+    if right_index < len(right):
+        result += right[right_index:]
+    return result
+
+
+def merge_sort(array):
+    if len(array) < 2:
+        return array
+    mid = len(array)/2
+    left = array[:mid]
+    right = array[mid:]
+    left = merge_sort(left)
+    right = merge_sort(right)
+    return list(merge(left, right))
+
--- tests/test_sort.py
+++ tests/test_sort.py
@@ -2,14 +2,15 @@ from . import helpers
 from lib import sort
 
 
-SORTING_ALGORITHMS = [sort.quicksort]
+SORTING_ALGORITHMS = ['quicksort', 'merge_sort']
 
 
 def test_sorts_small_list():
     unsorted_arrays = [
         [5,4], [3,1,2], [4.2,18.9,2.1], [300,9,1], [1,4,8,3]
     ]
-    for function in SORTING_ALGORITHMS:
+    for function_name in SORTING_ALGORITHMS:
+        function = getattr(sort, function_name)
         for array in unsorted_arrays:
             helpers.expect_equal(function(array), sorted(array))
 
@@ -18,7 +19,8 @@ def test_sorts_duplicates():
     unsorted_arrays = [
         [3,1,5,5,2,8,6,5], [1,1,1], [0,-1,-3,0,-1,7]
     ]
-    for function in SORTING_ALGORITHMS:
+    for function_name in SORTING_ALGORITHMS:
+        function = getattr(sort, function_name)
         for array in unsorted_arrays:
             helpers.expect_equal(function(array), sorted(array))
 
@@ -29,6 +31,7 @@ def test_sorts_larger_lists():
         [9,4,2,1,23,5,43,89,0,-12,-500],
         [-25, 0, 9.3, 2.1, 2.04, -6, 13, 18],
     ]
-    for function in SORTING_ALGORITHMS:
+    for function_name in SORTING_ALGORITHMS:
+        function = getattr(sort, function_name)
         for array in unsorted_arrays:
             helpers.expect_equal(function(array), sorted(array))
