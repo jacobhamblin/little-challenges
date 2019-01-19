78a1d4d313e19df3e3f12d58486a8139e69ae01c
Jacob Hamblin <jacob@hamblin.cc>
2019-01-19 11:54:28 -0700

    kth_smallest_in_place

--- lib/unsorted_list.py
+++ lib/unsorted_list.py
@@ -15,6 +15,28 @@ def kth_smallest(unsorted_list, k):
     else:
         return first_item
 
+def kth_smallest_in_place(arr, k):
+    if k > len(arr):
+        return None
+    if len(arr) == 1:
+        return arr[0]
+    pivot_index = 0
+    for ind in xrange(1, len(arr)):
+        if arr[ind] < arr[pivot_index]:
+            val = arr.pop(ind)
+            arr.insert(0, val)
+            pivot_index += 1
+    pivot_count = arr.count(arr[pivot_index])
+    if k < pivot_index + 1:
+        return kth_smallest_in_place(arr[:pivot_index], k)
+    elif k > pivot_index + pivot_count:
+        return kth_smallest_in_place(
+            arr[pivot_index + pivot_count:],
+            k - (pivot_index + pivot_count)
+        )
+    else:
+        return arr[pivot_index]
+
 def kth_largest(unsorted_list, k):
     if k > len(unsorted_list):
         return None
--- tests/test_unsorted_list.py
+++ tests/test_unsorted_list.py
@@ -1,20 +1,30 @@
 from . import helpers
 from lib import unsorted_list
 
+KTH_SMALLEST_FUNCTIONS = ['kth_smallest', 'kth_smallest_in_place']
 
 def test_kth_smallest_edges():
-    func = unsorted_list.kth_smallest
-    helpers.expect_equal(func([3], 2), None)
-    helpers.expect_equal(func([3, 3, 4], 1), 3)
-    helpers.expect_equal(func([], 1), None)
-    helpers.expect_equal(func([3, 3, 4, 5, 3, 5, 3], 1), 3)
+    for function_name in KTH_SMALLEST_FUNCTIONS:
+        func = getattr(unsorted_list, function_name)
+        helpers.expect_equal(func([3], 2), None)
+        helpers.expect_equal(func([3, 3, 4], 1), 3)
+        helpers.expect_equal(func([], 1), None)
+        helpers.expect_equal(func([3, 3, 4, 5, 3, 5, 3], 1), 3)
 
 def test_kth_smallest_basics():
-    func = unsorted_list.kth_smallest
-    helpers.expect_equal(func([3], 1), 3)
-    helpers.expect_equal(func([3,1,2], 1), 1)
-    helpers.expect_equal(func([3,1,2,-1,4], 1), -1)
-    helpers.expect_equal(func([3,1,2,-1,4], 3), 2)
+    for function_name in KTH_SMALLEST_FUNCTIONS:
+        func = getattr(unsorted_list, function_name)
+        helpers.expect_equal(func([3], 1), 3)
+        helpers.expect_equal(func([3,1,2], 1), 1)
+        helpers.expect_equal(func([3,1,2,-1], 1), -1)
+        helpers.expect_equal(func([3,1,2,-1], 2), 1)
+        helpers.expect_equal(func([3,1,2,-1], 3), 2)
+        helpers.expect_equal(func([3,1,2,-1], 4), 3)
+        helpers.expect_equal(func([3,1,2,-1,4], 1), -1)
+        helpers.expect_equal(func([3,1,2,-1,4], 2), 1)
+        helpers.expect_equal(func([3,1,2,-1,4], 3), 2)
+        helpers.expect_equal(func([3,1,2,-1,4], 4), 3)
+        helpers.expect_equal(func([3,1,2,-1,4], 5), 4)
     
 def test_kth_largest_edges():
     func = unsorted_list.kth_largest
