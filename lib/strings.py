dc630c9fed2890b67c1a3151f12610b5946f0fa4
Jacob Hamblin <jacob@hamblin.cc>
2019-02-02 17:44:01 -0700

    my approach

--- lib/strings.py
+++ lib/strings.py
@@ -37,3 +37,36 @@ def longest_substring_without_duplicate(string):
             current += 1
         i += 1
     return longest if longest > current else current
+
+# a-z -> trivial
+# . == wildcard -> trivial
+# (bbc, b*bc)
+def regex_match(s, pattern):
+    s_index = 0
+    p_index = 0
+    if len(s) and not len(pattern):
+        return False
+    while p_index < len(pattern):
+        if p_index + 1 < len(pattern) and pattern[p_index + 1] == '*':
+            new_str = 'a' + s[s_index:]
+            increment_to_s_index = 0
+            while len(new_str[1:]):
+                increment_to_s_index += 1
+                new_str = new_str[1:]
+                match = regex_match(new_str, pattern[p_index + 2:])
+                if match:
+                    return True
+            if not len(new_str):
+                return False
+            p_index += 1
+            s_index += (increment_to_s_index - 1)
+
+        elif pattern[p_index] == '.':
+            if s_index >= len(s):
+                return False
+        else:
+            if s[s_index] != pattern[p_index]:
+                return False
+        s_index += 1
+        p_index += 1
+    return True
--- tests/test_strings.py
+++ tests/test_strings.py
@@ -2,26 +2,40 @@ from . import helpers
 from lib import strings
 
 
+expect = helpers.expect_equal
+
+
 def test_is_anagram_edges():
-    helpers.expect_equal(strings.is_anagram('', ''), True)
-    helpers.expect_equal(strings.is_anagram('asd', ''), False)
-    helpers.expect_equal(strings.is_anagram('asd', 3), False)
-    helpers.expect_equal(strings.is_anagram(1, {}), False)
-    helpers.expect_equal(strings.is_anagram(True, 'asd'), False)
+    expect(strings.is_anagram('', ''), True)
+    expect(strings.is_anagram('asd', ''), False)
+    expect(strings.is_anagram('asd', 3), False)
+    expect(strings.is_anagram(1, {}), False)
+    expect(strings.is_anagram(True, 'asd'), False)
 
 def test_is_anagram_normal_cases():
-    helpers.expect_equal(strings.is_anagram('asd', 'qwe'), False)
-    helpers.expect_equal(strings.is_anagram('assd', 'asdd'), False)
-    helpers.expect_equal(strings.is_anagram('asds', 'sads'), True)
-    helpers.expect_equal(
+    expect(strings.is_anagram('asd', 'qwe'), False)
+    expect(strings.is_anagram('assd', 'asdd'), False)
+    expect(strings.is_anagram('asds', 'sads'), True)
+    expect(
         strings.is_anagram('i am a friend', 'a friend i am'),
         True
     )
-    helpers.expect_equal(strings.is_anagram('re sd', 'dse r'), True)
+    expect(strings.is_anagram('re sd', 'dse r'), True)
 
 def test_longest_substring_without_duplicate():
     func = strings.longest_substring_without_duplicate
-    helpers.expect_equal(func('abcbabccb'), 3)
-    helpers.expect_equal(func('twtadst'), 5)
-    helpers.expect_equal(func('mbmbookl'), 3)
-    helpers.expect_equal(func(''), 0)
+    expect(func('abcbabccb'), 3)
+    expect(func('twtadst'), 5)
+    expect(func('mbmbookl'), 3)
+    expect(func(''), 0)
+
+def test_regex_match():
+    func = strings.regex_match
+    expect(func('ab', 'ab'), True)
+    expect(func('abc', 'a.c'), True)
+    expect(func('bbc', 'b*bc'), True)
+    expect(func('bbc', 'b.*'), True)
+    expect(func('', '.*'), True)
+    expect(func('', '.'), False)
+    expect(func('ab', 'ba'), False)
+
