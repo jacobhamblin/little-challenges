5103c9f29f26fa52664cb08ef226b6776f058b24
Jacob Hamblin <jacob@hamblin.cc>
2019-02-24 12:52:09 -0700

    more failing tests

--- lib/meetings.py
+++ lib/meetings.py
@@ -1,2 +1,5 @@
 def most_meetings(meetings, availability):
     return meetings
+
+def most_time(meetings, availability):
+    return meetings
--- tests/test_meetings.py
+++ tests/test_meetings.py
@@ -27,3 +27,25 @@ def test_most_meetings():
         {'who': 'Eliza', 'duration': 3.5},
         {'who': 'Tony', 'duration': 4},
     ])
+
+def test_most_time():
+    data_set = [
+        {'who': 'Marco', 'duration': 5},
+        {'who': 'Ustad', 'duration': 6},
+        {'who': 'Tony', 'duration': 2},
+        {'who': 'Eliza', 'duration': 1},
+        {'who': 'Jill', 'duration': 4},
+    ]
+    expect(meetings.most_time(data_set, 8), [
+        {'who': 'Ustad', 'duration': 4},
+        {'who': 'Tony', 'duration': 2},
+    ])
+    data_set = [
+        {'who': 'Amara', 'duration': 5},
+        {'who': 'Tony', 'duration': 4},
+        {'who': 'Eliza', 'duration': 3.5},
+    ]
+    expect(meetings.most_time(data_set, 8), [
+        {'who': 'Tony', 'duration': 4},
+        {'who': 'Eliza', 'duration': 3.5},
+    ])
