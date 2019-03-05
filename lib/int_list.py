b2c6a31803901298e875d1f570a8fc3325edd8c1
Jacob Hamblin <jacob@hamblin.cc>
2019-03-04 19:50:45 -0700

    close

--- lib/int_list.py
+++ lib/int_list.py
@@ -1,3 +1,5 @@
+import sys
+
 def get_mode(arr):
     k = len(arr)
     for i in range(0, k):
@@ -41,3 +43,42 @@ def num_subsets_add_to_k(nums, k):
             combinations[index].append(num)
             combinations.append(cloned)
     return sum([1 if sum(combo) == k else 0 for combo in combinations])
+
+# {1,2,3}, [4,3,2,6,1,7] => [3,2,6,1]
+def shortest_continuous_subarray(int_set, random_ints):
+    if not len(int_set) or not len(random_ints): return []
+    best = sys.maxsize
+    best_indices = []
+    for start_index in xrange(len(random_ints)):
+        ints_seen = set()
+        for current_index in xrange(start_index, len(random_ints)):
+            current_num = random_ints[current_index]
+            if current_num in int_set:
+                ints_seen.add(current_num)
+                if len(ints_seen) == len(int_set):
+                    best = min(best, current_index - start_index)
+                    if current_index - start_index == best:
+                        best_indices = [start_index, current_index]
+                    break
+    if not len(best_indices): return []
+    return random_ints[best_indices[0]:best_indices[1] + 1]
+
+def shortest_continuous_subarray_linear(int_set, random_ints):
+    if not len(int_set) or not len(random_ints): return []
+    best = sys.maxsize
+    best_indices = []
+    ints_seen = {}
+    first_index = sys.maxsize
+    last_index = -sys.maxsize
+    for index in xrange(len(random_ints)):
+        num = random_ints[index]
+        if num in int_set:
+            if index < first_index: first_index = index
+            if index > last_index: last_index = index
+            ints_seen[num] = index
+            if len(int_set) == len(ints_seen):
+                best = min(best, last_index - first_index)
+                if last_index - first_index == best:
+                    best_indices = [first_index, last_index]
+    if not len(best_indices): return []
+    return random_ints[best_indices[0]:best_indices[1] + 1]
--- tests/test_int_list.py
+++ tests/test_int_list.py
@@ -1,20 +1,14 @@
 from . import helpers
 from lib import int_list
 
+expect = helpers.expect_equal
 
 def test_get_mode():
-    helpers.expect_equal(int_list.get_mode([1,2,3,2]), 2)
-    helpers.expect_equal(
-        int_list.get_mode([5,2,3,1,2,5,7,5,4]),
-        5
-    )
-    helpers.expect_equal(
-        int_list.get_mode([2,4,6,3,4,1]),
-        4
-    )
+    expect(int_list.get_mode([1,2,3,2]), 2)
+    expect(int_list.get_mode([5,2,3,1,2,5,7,5,4]), 5)
+    expect(int_list.get_mode([2,4,6,3,4,1]), 4)
 
 def test_num_subsets_add_to_k():
-    expect = helpers.expect_equal
     function_names = ['num_subsets_add_to_k', 'num_subsets_add_to_k_memoized']
     for function_name in function_names:
         func = getattr(int_list, function_name)
@@ -29,3 +23,12 @@ def test_num_subsets_add_to_k():
         expect(func(nums, 10), 2)
         expect(func(nums, 12), 2)
         expect(func(nums, 16), 2)
+
+def test_shortest_continuous_subarray():
+    function_names = ['shortest_continuous_subarray', 'shortest_continuous_subarray_linear']
+    for function_name in function_names:
+        func = getattr(int_list, function_name)
+        expect(func({1,2,3}, [4,2,6,3,1,8]), [2,6,3,1])
+        expect(func({1,2}, [4,2,6,3,1,8,3,4,1,3,2,0,0,1]), [1,3,2])
+        expect(func({1,2,3,4}, [3,5,7,1,8,2,3]), [])
+    
