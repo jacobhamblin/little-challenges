e0ad6514c76d34e9100e94e582dcb88b6ac7c23b
Jacob Hamblin <jacob@hamblin.cc>
2021-05-12 10:59:26 -0700

    black pre-commit

--- lib/change.py
+++ lib/change.py
@@ -8,6 +8,7 @@ def get_combinations(value, coins, coin_counts, combinations):
             new_value = value - coin
             get_combinations(new_value, coins, new_coin_counts, combinations)
 
+
 def fewest_coins(value, coins):
     coin_counts = [0 for coin in coins]
     if value == 0:
@@ -18,12 +19,13 @@ def fewest_coins(value, coins):
     counts = [reduce(get_sum, nums) for nums in combinations]
     return combinations[counts.index(min(counts))]
 
+
 def get_staircase_combinations(
-        steps_remaining,
-        step_increments,
-        sequence,
-        combinations,
-    ):
+    steps_remaining,
+    step_increments,
+    sequence,
+    combinations,
+):
     if not steps_remaining:
         combinations.append(sequence)
     for increment in step_increments:
@@ -32,21 +34,15 @@ def get_staircase_combinations(
         new_sequence = list(sequence)
         new_sequence.append(increment)
         get_staircase_combinations(
-            steps_remaining - increment,
-            step_increments,
-            new_sequence,
-            combinations
+            steps_remaining - increment, step_increments, new_sequence, combinations
         )
+
 
 def staircase(total_steps, step_increments):
     combinations = []
-    get_staircase_combinations(
-        total_steps,
-        step_increments,
-        [],
-        combinations
-    )
+    get_staircase_combinations(total_steps, step_increments, [], combinations)
     return len(combinations)
+
 
 def fewest_coins_num_used(val, coins):
     if val == 0:
